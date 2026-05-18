03013eac93d24d836c3e400fb6dfa130be0db25f
cmedrano <cristian.ariel.medrano@gmail.com>
2026-05-18 16:05:07 -0300

    nueva clase para el modelo del mes

---
+++ Areas/Accounting/Data/Model/MesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PresupuestoMVC.Areas.Accounting.Data.Model
+{
+    public class MesViewModel
+    {
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+    }
+}
