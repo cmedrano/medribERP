f9ad2754463f083d2ae52ecff05a4adc63b6fc39
medranocrisariel <cristian.ariel.medrano@gmail.com>
2026-05-28 14:55:02 -0300

    implementacion base para Reportes

---
+++ Areas/Accounting/Controllers/ReportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresupuestoMVC.Areas.Accounting.Controllers
+{
+    [Area("Accounting")]
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
