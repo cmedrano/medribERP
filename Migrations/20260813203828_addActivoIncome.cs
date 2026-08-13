bccdc8a82397183c6737ba3b996bd5cebbe18ca7
cmedrano <cristian.ariel.medrano@gmail.com>
2026-08-13 17:50:38 -0300

    se agrego activo para income

---
+++ Migrations/20260813203828_addActivoIncome.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresupuestoMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class addActivoIncome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "activo",
+                table: "income_transfers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "activo",
+                table: "income_transfers");
+        }
+    }
+}
